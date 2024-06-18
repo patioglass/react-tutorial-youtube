ae178c4695aa769809fa217f22df0e0f200961da
patioglass <patiopatimon@gmail.com>
2024-06-18 17:05:12 +0900

    todoをバケツリレーする

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { useState } from "react";
 import TodoList from "./TodoList";
 
 function App() {
-  const [todos, setTodos] = useState();
+  const [todos, setTodos] = useState(["Todo1", "Todo2"]);
 
   return (
     <>
-      <TodoList />
+      <TodoList todos={todos}/>
       <input type="text" />
       <button>タスクを追加</button>
       <button>完了したタスクの削除</button>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Todo from './Todo'
 
-const TodoList = () => {
-  return (
-    <div>TodoList</div>
+const TodoList = ({ todos }) => {
+  return todos.map((todo) => 
+    <Todo todo={todo} key={todo} />
   )
 }
 
